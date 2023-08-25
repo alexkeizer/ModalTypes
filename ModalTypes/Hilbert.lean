d39179d10bf04b62eaef14a317ed4f66262a3317
Alex Keizer <alex@keizer.dev>
2023-08-25 23:33:16 +0100

    Hilbert Proofs

--- ModalTypes/Hilbert.lean
+++ ModalTypes/Hilbert.lean
@@ -1,37 +1,47 @@
 import ModalTypes.Type
 import Mathlib.Data.Vector.Basic
 
-def TypeScheme : Nat → Type
-  | 0 => Ty
-  | n+1 => Ty → TypeScheme n
+open TypeScheme
 
-def TypeScheme.apply (t : TypeScheme n) (args : Vector Ty n) : Ty :=
-  by 
-    cases args using Vector.inductionOn
-    next => exact t -- nil
-    next x args _ => exact apply (t x) args -- cons
+abbrev Axioms := List (Σn, TypeScheme n)
 
-
-namespace HilbertProof
-
-
-inductive Hypothesis (Γ : List (Σn, TypeScheme n)) (t : ) : Type
-  /-- An axiom -/
-  | axiomI (i : Fin ax)
-  /-- A previously proven result -/
-  | prevI (i : Fin n)
-
-inductive Proof (ax n : Nat)
-  /-- substitution -/
-  | sub {m} (scheme : Index ax n) (sub : Vector (Index ax n) m)
-
-end HilbertProof
-
+namespace Axioms
 
 /--
-  Treating types just as formulas, a `HilbertProof t` shows that `t` is an axiom.
-  A proof is 
+  Treating types just as formulas, a `Proof axioms t` shows that `t` is a theorem that follows
+  from the given `axioms`.
+  This is a Hilbert-style axiomatic proof.
 -/
-inductive HilbertProof : List (Σn, TypeScheme n) → TypeScheme n → Prop
-  | quantify (t : TypeScheme (n+1)) : (∀ u, HilbertProof <| t u) → HilbertProof t 
-  | 
+inductive Proof (axioms : Axioms) : {n : Nat} → TypeScheme n → Prop
+  | axiom : (∃i, axioms.get i = ⟨n, t⟩) → Proof axioms t
+  | modusPonens {A B : TypeScheme n} : 
+      Proof axioms ⟪A → B⟫ → Proof axioms A → Proof axioms B
+  | substitution (f : Fin n → TypeScheme m) : Proof axioms A → Proof axioms (A.substitute f)
+  /--
+    Rule of necessitation: if `A` is a theorem, then so is `□A`.
+    Note that this is different from having `A → □A` as an axiom!
+  -/
+  | necessitation : Proof axioms A → Proof axioms ⟪ □A ⟫
+
+
+
+/-- The basic axioms of propositional, intuitionistic logic 
+    https://en.wikipedia.org/wiki/Intuitionistic_logic
+-/
+def propLogic : Axioms := [
+  ⟨2, ⟪ %0 → %1 → %0 ⟫⟩,  -- THEN-1
+  ⟨3, ⟪ (%0 → (%1 → %2)) → (%0 → %1) → (%0 → %2) ⟫⟩, -- THEN-2
+  ⟨2, ⟪ %0 × %1 → %0 ⟫⟩, -- AND-1
+  ⟨2, ⟪ %0 × %1 → %1 ⟫⟩, -- AND-2
+  ⟨2, ⟪ %0 → %1 → %0 × %1 ⟫⟩, -- AND-3
+  ⟨2, ⟪ %0 → %0 + %1 ⟫⟩, -- OR-1
+  ⟨2, ⟪ %1 → %0 + %1 ⟫⟩, -- OR-2
+  ⟨2, ⟪ (%0 → %2) → (%1 → %2) → (%0 + %1 → %2)⟫⟩ -- OR-3
+  -- no False, since we don't have an empty type
+]
+
+def K : Axioms := propLogic ++ [
+  ⟨2, ⟪ □(%0 → %1) → □%0 → □%1 ⟫⟩
+]
+
+end Axioms
--- ModalTypes/Type.lean
+++ ModalTypes/Type.lean
@@ -1,42 +1,69 @@
+import Mathlib.Data.Fin.Basic
 
 
 
 inductive Modality
   | Box
   | Dia
+  deriving Repr, DecidableEq
 
-inductive Ty
+inductive TypeScheme (n : Nat)
   /-- `fun x y` is the type of functions from `x` to `y` -/
-  | fun : Ty → Ty → Ty
+  | fun : TypeScheme n → TypeScheme n → TypeScheme n
   /-- `pair x y` is a pair of `x` and `y` -/
-  | pair : Ty → Ty → Ty
+  | pair : TypeScheme n → TypeScheme n → TypeScheme n
   /-- `sum x y` is either an `x`, or a `y` -/
-  | sum : Ty → Ty → Ty
+  | sum : TypeScheme n → TypeScheme n → TypeScheme n
   /-- `mod m x` is a type `x` under modality `m` -/
-  | mod : Modality → Ty → Ty
+  | mod : Modality → TypeScheme n → TypeScheme n
+  /-- A type variable -/
+  | var : Fin n → TypeScheme n
+  deriving Repr, DecidableEq
 
+def Ty := TypeScheme 0
 
-namespace Ty
+namespace TypeScheme
+
+variable (f : Fin n → TypeScheme m) in
+/-- Apply a transformation to variables, leaving other constructors unchanges -/
+def substitute : TypeScheme n → TypeScheme m
+  | .fun A B => .fun (substitute A) (substitute B)
+  | .pair A B => .pair (substitute A) (substitute B)
+  | .sum A B => .sum (substitute A) (substitute B)
+  | .mod m A => .mod m (substitute A)
+  | .var v => f v
+
+def substituteAt (i : Fin (n+1)) (substituent : TypeScheme n) : TypeScheme (n+1) → TypeScheme n :=
+  substitute <| match n with 
+    -- if `n=0`, then there is only one possible variable `i : Fin (n+1)`, 
+    -- So, we change all variables to the substituent
+    | 0 => fun _ => substituent
+    | _+1 => fun j => 
+      if i = j then
+        substituent
+      else
+        .var <| .predAbove (i.castPred) j
 
 declare_syntax_cat mod_ty
 scoped syntax "⟪" mod_ty "⟫" : term
 
-syntax:66 mod_ty:65 " → " mod_ty:66 : mod_ty
-syntax mod_ty " + " mod_ty : mod_ty
-syntax mod_ty " × " mod_ty : mod_ty
-syntax:99 "□" mod_ty:100 : mod_ty
-syntax:99 "◇" mod_ty:100 : mod_ty
+syntax:46 mod_ty:45 " → " mod_ty:46 : mod_ty
+syntax:56 mod_ty:56 " + " mod_ty:55 : mod_ty
+syntax:66 mod_ty:66 " × " mod_ty:65 : mod_ty
+syntax:99 "□" mod_ty:99 : mod_ty
+syntax:99 "◇" mod_ty:99 : mod_ty
 syntax "(" mod_ty ")" : mod_ty
 syntax ident : mod_ty
+syntax "%"term:100 : mod_ty
 
 macro_rules
-  | `(⟪ $A → $B ⟫) => `(Ty.fun ⟪ $A ⟫ ⟪ $B ⟫)
-  | `(⟪ $A × $B ⟫) => `(Ty.pair ⟪ $A ⟫ ⟪ $B ⟫)
-  | `(⟪ $A + $B ⟫) => `(Ty.sum ⟪ $A ⟫ ⟪ $B ⟫)
-  | `(⟪ □$A ⟫) => `(Ty.mod .Box ⟪ $A ⟫)
-  | `(⟪ ◇$A ⟫) => `(Ty.mod .Dia ⟪ $A ⟫)
+  | `(⟪ $A → $B ⟫) => `(TypeScheme.fun ⟪ $A ⟫ ⟪ $B ⟫)
+  | `(⟪ $A × $B ⟫) => `(TypeScheme.pair ⟪ $A ⟫ ⟪ $B ⟫)
+  | `(⟪ $A + $B ⟫) => `(TypeScheme.sum ⟪ $A ⟫ ⟪ $B ⟫)
+  | `(⟪ □$A ⟫) => `(TypeScheme.mod .Box ⟪ $A ⟫)
+  | `(⟪ ◇$A ⟫) => `(TypeScheme.mod .Dia ⟪ $A ⟫)
   | `(⟪ ($A) ⟫) => `(⟪$A⟫)
   | `(⟪ $A:ident ⟫) => `($A:term)
-  
+  | `(⟪ %$n:term ⟫) => `(TypeScheme.var $n)
 
-end Ty
+end TypeScheme
